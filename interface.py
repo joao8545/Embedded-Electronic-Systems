622848e6840305174ae842366157fe35958d3396
Joao Anastacio <almeida.jar97@gmail.com>
2023-05-13 18:36:58 -0300

    added functions to deal with orientation

--- interface.py
+++ interface.py
@@ -7,6 +7,30 @@ import time
 from datetime import datetime
 #from sense_hat import SenseHat
 
+def euler_angles_to_rotation_matrix(roll, pitch, yaw):
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(roll), -np.sin(roll)],
+                    [0, np.sin(roll), np.cos(roll)]])
+                    
+    R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
+                    [0, 1, 0],
+                    [-np.sin(pitch), 0, np.cos(pitch)]])
+                    
+    R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
+                    [np.sin(yaw), np.cos(yaw), 0],
+                    [0, 0, 1]])
+                    
+    # Combine the three rotation matrices to get the final rotation matrix
+    R = np.dot(R_z, np.dot(R_y, R_x))
+    return R
+
+
+def local_to_global_direction(rotation_matrix, local_movement):
+    # Multiply the rotation matrix by the local movement vector
+    global_direction = np.dot(rotation_matrix, local_movement)
+    return global_direction
+
+
 MOCK=True
 class DataFrame:
     def __init__(self,acel=None,gyro=None,orientation=None,pressure=None,mag=None,timestamp=None) -> None:
